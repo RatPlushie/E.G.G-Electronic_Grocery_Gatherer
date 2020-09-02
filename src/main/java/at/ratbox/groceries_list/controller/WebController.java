077361f7a4393b790a4b55d43b0c7443195696ad
aki <kieran.mortimer@gmail.com>
2020-09-02 12:30:06 +0200

    Create, delete, delete all buttons logic implemented

--- src/main/java/at/ratbox/groceries_list/controller/WebController.java
+++ src/main/java/at/ratbox/groceries_list/controller/WebController.java
@@ -1,10 +1,14 @@
 package at.ratbox.groceries_list.controller;
 
+import at.ratbox.groceries_list.exceptions.ResourceNotFoundException;
 import at.ratbox.groceries_list.model.Grocery;
 import at.ratbox.groceries_list.repository.GroceryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,11 +28,45 @@ public class WebController {
 		// Adding the list to a thymeleaf accessible attribute
 		model.addAttribute("groceryList", groceryList);
 
-
+		// Adding the Grocery class to the thymeleaf attributes
+		model.addAttribute("newGrocery", new Grocery());
 
 		return "index";
 	}
 
+	// Item creation
+	@PostMapping("/create")
+	public String createItem(@ModelAttribute("newGrocery") Grocery grocery) {
+		// POST request validation
+		if (grocery.getName().isEmpty() || grocery.getQuantity() == 0) {
+			// TODO - write modal prompt for invalid form submission
 
+			return "redirect:/";
+		}
 
+		groceryRepository.save(grocery);
+
+		return "redirect:/";
+	}
+
+	// Item delete button
+	@RequestMapping("/delete/{id}")
+	public String deleteItem(@PathVariable(value = "id") long groceryID) {
+		// Locating the correct item via id
+		Grocery grocery = groceryRepository.findById(groceryID).orElseThrow(() -> new ResourceNotFoundException("Grocery", "Id", groceryID));
+
+		// Removing it from the list
+		groceryRepository.delete(grocery);
+
+		// Returning to the grocery list
+		return "redirect:/";
+	}
+
+	// Delete ALL button
+	@RequestMapping("/deleteALL")
+	public String deleteAllItems() {
+		groceryRepository.deleteAll();
+
+		return "redirect:/";
+	}
 }
